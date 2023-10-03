c41cf98448ea3b9ec0aabe700cba353ef9a053f5
Malav27504 <68182661+Malav27504@users.noreply.github.com>
2023-10-03 15:14:46 -0400

    Update storeData.php

--- PHP-Assignment1/storeData.php
+++ PHP-Assignment1/storeData.php
@@ -29,10 +29,10 @@
 
     $hostname = "localhost";
     $username="root";
-    // $password=" ";
+    $password="admin";
     $database="phpassignment1";
 
-    $conn=mysqli_connect($hostname,$username,NULL,$database);
+    $conn=mysqli_connect($hostname,$username,$password,$database);
 
     if(!$conn)
     {
